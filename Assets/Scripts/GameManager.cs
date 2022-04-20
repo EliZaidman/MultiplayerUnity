86d34068525e2b1d5c357ed448570dd2def4d829
Eli Zaidman <Rxelik5@gmail.com>
2022-04-20 16:06:31 +0300

    FirstPool

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject CurrentEnemey;
+
+    //Counter
+    public float counter;
+    //Time Between Spawns
+    public float delay;
+    //Amount
+    public int enemeySpawns;
+    //FailSafe to ensure exacly as wanted
+    public bool spawned;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        SpawnEnemies(enemeySpawns);
+
+    }
+
+    void SpawnEnemies(int amount)
+    {
+        counter = counter + Time.deltaTime;
+        if (counter > delay && spawned)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                CurrentEnemey = Instantiate(enemy, spawnPoint.transform.position, spawnPoint.transform.rotation);
+            }
+            spawned = false;
+            counter = 0;
+        }
+        else
+        {
+            spawned = true;
+        }
+    }
+}
